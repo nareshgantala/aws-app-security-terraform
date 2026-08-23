ee06ddb14ab0c8415391180c8c33ef82b8b1b59f
Naresh Gantala <nareshgantala@gmail.com>
2026-08-23 11:29:46 +0530

    ssh ingress disabled

--- securityGroup.tf
+++ securityGroup.tf
@@ -27,13 +27,13 @@ resource "aws_vpc_security_group_ingress_rule" "alb-ec2-ingress" {
   to_port                      = 8080
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh-ingress" {
-  security_group_id = aws_security_group.ec2_sg.id
-  cidr_ipv4         = "172.16.0.0/16"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "ssh-ingress" {
+#   security_group_id = aws_security_group.ec2_sg.id
+#   cidr_ipv4         = "172.16.0.0/16"
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
 
 resource "aws_vpc_security_group_ingress_rule" "alb-public-ingress" {
   security_group_id = aws_security_group.alb_sg.id
